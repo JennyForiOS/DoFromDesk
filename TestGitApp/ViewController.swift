848626e5b9a6e804e9a92d9f0c7fe91d8760e466
JennyLoveFish <1272242576@qq.com>
2021-02-25 16:24:59 +0900

    Initial Commit

---
+++ TestGitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by HL株式会社 on 2021/02/25.
+//  Copyright © 2021 text. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
